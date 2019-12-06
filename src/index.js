08452eb80c7d9a1125b1a5ade2a51c079bee1fc1
rbpaul2 <rbpaul@umich.edu>
2019-12-06 11:01:34 -0800

    Make GIF search

--- src/index.js
+++ src/index.js
@@ -1,28 +1,95 @@
 import React from "react";
 import { render } from "react-dom";
-import DevTools from "mobx-react-devtools";
+import {observer} from "mobx-react";
+import _ from "lodash";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-import TodoList from "./components/TodoList";
-import TodoListModel from "./models/TodoListModel";
-import TodoModel from "./models/TodoModel";
+const autofills = [
+    'about',
+    'above',
+    'across',
+    'app',
+    'apple',
+    'appreciate',
+    'bad',
+    'ball',
+    'balloon',
+    'bell',
+    'cat',
+];
 
-const store = new TodoListModel();
+@observer
+class GiphySearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            isLoading: false,
+            gifs: [],
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: _.get(event, 'target.value', '') });
+    }
+
+    handleSubmit(event) {
+        const searchString = encodeURI(this.state.value);
+        const url = `http://api.giphy.com/v1/gifs/search?q=${searchString}&api_key=DLCVuTK6KZExOS7JoMq82bi5MaI6EbWO&limit=1`;
+        fetch(url)
+            .then(data => data.json())
+            .then(res => {
+                console.log(res);
+                this.setState({ gifs: _.get(res, 'data') });
+            })
+            .catch(err => console.log(err));
+        event.preventDefault();
+
+    }
+
+    render() {
+        return (
+            <div className={'container'}>
+                <form onSubmit={this.handleSubmit}>
+                    <div className={'content'}>
+                        <div className={'header'}>
+                            <Autocomplete
+                                freeSolo
+                                options={autofills}
+                                inputValue={this.state.value}
+                                onInputChange={this.handleChange}
+                                renderInput={params => (
+                                    <TextField {...params} label="Search for GIF" margin="normal" variant="outlined" fullWidth />
+                                )}
+                            />
+                            <input type="submit" value="GO" />
+                        </div>
+                        <div className={'results'}>
+                            {this.state.gifs.map(g => {
+                                return <img src={_.get(g, 'images.original.url')} />;
+                            })}
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+    },
+});
 
 render(
-  <div>
-    <DevTools />
-    <TodoList store={store} />
-  </div>,
+    <ThemeProvider theme={theme}>
+        <GiphySearch/>
+    </ThemeProvider>,
   document.getElementById("root")
 );
-
-store.addTodo("Get Coffee");
-store.addTodo("Write simpler code");
-store.todos[0].finished = true;
-
-setTimeout(() => {
-  store.addTodo("Get a cookie as well");
-}, 2000);
-
-// playing around in the console
-window.store = store;
